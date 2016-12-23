3f29868b111405fb30b731c391e52dc9bf970bba
Emad <Emad@Emad-Pc>
2016-12-23 15:32:18 +0200

    last update with comments

--- src/testsimplecircle/TestSimpleCircle.java
+++ src/testsimplecircle/TestSimpleCircle.java
@@ -10,16 +10,20 @@ package testsimplecircle;
  * @author Emad
  */
 public class TestSimpleCircle {
-    
-    double radius;
-    TestSimpleCircle(){
+    // datafield radius 
+    double radius;     
+    //default constructor
+    TestSimpleCircle(){  
         
     }
-    public TestSimpleCircle(double newRadius) {
+    //constructor to get radius
+    public TestSimpleCircle(double newRadius) {   
         this.radius = newRadius;
     }
     public static void main(String[] args) {
-        TestSimpleCircle circle1 = new TestSimpleCircle(1);
+        //we can pass radius to constructor or setRadius method
+        TestSimpleCircle circle1 = new TestSimpleCircle();
+        circle1.setRadius(1);
         circle1.printDetails();
         TestSimpleCircle circle2 = new TestSimpleCircle(25);
         circle2.printDetails();
@@ -28,15 +32,19 @@ public class TestSimpleCircle {
         TestSimpleCircle circle4 = new TestSimpleCircle(125);
         circle4.printDetails();   
     }
-    public double getArea(){
-        return Math.PI * Math.pow(radius, 2);
+    //this method calculate Area of circle
+    public double getArea(){  
+        return Math.PI * Math.pow(radius, 2); 
     }
-    public double getPriemeter(){
+    //this method calculate primeter (محيط ) of circle
+    public double getPriemeter(){ 
         return 2 * Math.PI * radius;
     }
+    //this method to set radius to calculate Area And primeter after create object
     public void setRadius(double newRadius){
        radius = newRadius;
     }
+    //print all result 
     public void printDetails(){
         System.out.println("Calculate Area and Priemter for Circle With Radius "+radius+" cm");
         System.out.println("Area      = "+getArea()+" cm^2");
